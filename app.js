4299596945f66c74552d0f2b39af254aaba23c85
Jaydeep Bhayani <jaydeepbhayani773@gmail.com>
2022-02-03 04:03:56 -0500

    task 6

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('./app_server/models/db');
+require('./app_api/models/db');
 
 const indexRouter = require('./app_server/routes/index');
-const usersRouter = require('./app_server/routes/users');
+const apiRouter = require('./app_api/routes/index');
+//const usersRouter = require('./app_server/routes/users'); //no need its created by system
 
 var app = express();
 
@@ -21,7 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', apiRouter);
+//app.use('/users', usersRouter); //no need its created by system
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
